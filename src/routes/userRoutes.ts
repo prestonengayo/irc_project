4b445a52985460bbf8424df4784107acf783d21e
ozdami_b <ozdami_b@etna-alternance.net>
2024-03-30 17:23:11 +0000

    removing test roots

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,5 +1,6 @@
 import { Router, Request, Response } from 'express';
 import { handleLogin, handleRegistration, showReset } from '../controllers/userController';
+import * as userController from '../controllers/userController';
 
 const router = Router();
 
@@ -7,5 +8,6 @@ router.post('/login', handleLogin); // call this function when check user login 
 router.post('/checkRegistration', handleRegistration); // call this function when check user registration info
 
 router.get('/reset', showReset);
+
 
 export default router;
