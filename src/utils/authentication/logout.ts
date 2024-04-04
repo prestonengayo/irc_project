80e33fd01950d48d3176ac63dcdb9c4da9f751a6
ozdami_b <ozdami_b@etna-alternance.net>
2024-04-04 13:37:30 +0000

    disconnect function added ( need to be reviewed with dev-pres branch merge)

--- src/utils/authentication/logout.ts
+++ src/utils/authentication/logout.ts
@@ -1,6 +1,22 @@
 import { Request, Response } from 'express';
-import User from '../../models/userModel';
 
 export const handleLogout = async (req: Request, res: Response) => {
+    try {
 
+        if (!req.session) {
+            return res.status(401).send('You are not connected');
+        }
+       
+        req.session.destroy((err) => {
+            if (err) {
+                console.error('An error occurred while trying to disconnect :', err);
+                return res.status(500).send('An error occurred while trying to disconnect');
+            }
+            
+            res.redirect('/');
+        });
+    } catch (error) {
+        console.error('An error occurred while trying to disconnect :', error);
+        res.status(500).send('An error occurred while trying to disconnect');
+    }
 }
