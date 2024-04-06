5db05b257daf092c074ca5e393bff410ee380462
ozdami_b <ozdami_b@etna-alternance.net>
2024-04-06 10:16:56 +0000

    update profile page

--- src/utils/authentication/profile.ts
+++ src/utils/authentication/profile.ts
@@ -11,15 +11,10 @@ export const profilePage = async (req: Request, res: Response) => {
         const user = await User.findById(userId);
 
         if (!user) {
-                    return res.status(404).send('User nor found.');
+            return res.status(404).send('User nor found.');
         }
 
-       
-        const users = await User.find();
-        
-        
-      
-        res.render('profile', { users, isAdmin: user.isAdmin, username: user.username, email: user.email, userId: user._id  });
+        res.render('profile', { username: user.username, email: user.email, userId: user._id  });
     } catch (error) {
         console.error('Erreur lors de la récupération du profil de l\'utilisateur:', error);
         res.status(500).send('Erreur lors de la récupération du profil de l\'utilisateur.');
